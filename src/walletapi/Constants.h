8f75b9d248ab002f771fec71baca0e22b0f27904
Bob <mrrovot@protonmail.com>
2020-08-02 21:23:08 -0600

    added matches property in ApiDispatcher regex

--- src/walletapi/Constants.h
+++ src/walletapi/Constants.h
@@ -17,8 +17,8 @@ namespace ApiConstants
 
     /* This is the equivalent of TRTL[a-zA-Z0-9]{95} but working for all coins */
     const std::string addressRegex =
-        std::string(WalletConfig::addressPrefix) + "[a-zA-Z0-9]{" + std::to_string(addressBodyLength) + "}";
+        "(" + std::string(WalletConfig::addressPrefix) + "[a-zA-Z0-9]{" + std::to_string(addressBodyLength) + "})";
 
     /* 64 char, hex */
-    const std::string hashRegex = "[a-fA-F0-9]{64}";
+    const std::string hashRegex = "([a-fA-F0-9]{64})";
 } // namespace ApiConstants
