342cc1146c456cb77e3db42f34558eef14e7fc28
Brandon Lehmann <brandonlehmann@gmail.com>
2020-08-16 14:09:13 -0400

    This moved behind the transaction private key now

--- src/rpc/RpcServer.cpp
+++ src/rpc/RpcServer.cpp
@@ -1152,7 +1152,7 @@ std::tuple<Error, uint16_t> RpcServer::getBlockTemplate(
 
     std::vector<uint8_t> blockBlob = CryptoNote::toBinaryArray(blockTemplate);
 
-    const auto transactionPublicKey = Utilities::getTransactionPublicKeyFromExtra(
+    const auto transactionPrivateKey = Utilities::getTransactionPrivateKeyFromExtra(
         blockTemplate.baseTransaction.extra
     );
 
@@ -1160,17 +1160,17 @@ std::tuple<Error, uint16_t> RpcServer::getBlockTemplate(
 
     if (reserveSize > 0)
     {
-        /* Find where in the block blob the transaction public key is */
+        /* Find where in the block blob the pool nonce value is */
         const auto it = std::search(
             blockBlob.begin(),
             blockBlob.end(),
-            std::begin(transactionPublicKey.data),
-            std::end(transactionPublicKey.data)
+            std::begin(transactionPrivateKey.data),
+            std::end(transactionPrivateKey.data)
         );
 
         /* The reserved offset is past the transactionPublicKey, then past
          * the extra nonce tags */
-        reservedOffset = (it - blockBlob.begin()) + sizeof(transactionPublicKey) + 2;
+        reservedOffset = (it - blockBlob.begin()) + sizeof(transactionPrivateKey) + 2;
 
         if (reservedOffset + reserveSize > blockBlob.size())
         {
